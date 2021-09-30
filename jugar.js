8308b205a0a2ffa2ac4022134eec5a00fdc1980a
Lidia A <90976607+LidiaA3@users.noreply.github.com>
2021-09-30 20:13:46 +0200

    Add files via upload

--- jugar.js
+++ jugar.js
@@ -7,34 +7,42 @@ var answer = (Math.floor(Math.random() * 100)) + 1;
 var no_of_guesses = 0;
 var guesses_num = [];
 
+function comprobarPulsadoEnter (evento) {
+    if (evento.code == 'Enter') {
+        comprobarLetraUsuario();
+    }
+}
+
 function play(){
   var user_guess = document.getElementById("guess").value;
+  document.getElementById("guess").value = "";
+  document.getElementById("guess").focus();
 
   if(user_guess < 1 || user_guess > 100){
-    alert("Please, enter  a number between 1 to 100");
+    alert("Por favor introduce un número del 1 al 100");
   }
   else{
     guesses_num.push(user_guess);
     no_of_guesses += 1;
 
       if(user_guess < answer){
-        msg1.innerHTML = "Your Guess is too low";
-        msg2.innerHTML = "No. of Guesses : " + no_of_guesses;
-        msg3.innerHTML = "Guessed number are:" + guesses_num;
+        msg1.innerHTML = "Tu intento es muy bajo";
+        msg2.innerHTML = "No. de intentos : " + no_of_guesses;
+        msg3.innerHTML = "Wrong Numbers:" + guesses_num;
       }
         else if(user_guess > answer){
-          msg1.innerHTML = "Your guess is too high";
-          msg2.innerHTML = "No. of guesses : " + no_of_guesses;
-          msg3.innerHTML = "Guessed number are: " + guesses_num;
+          msg1.innerHTML = "Tu intento es muy alto";
+          msg2.innerHTML = "No. de intentos : " + no_of_guesses;
+          msg3.innerHTML = "Wrong Numbers: " + guesses_num;
         }
         else if(user_guess == answer){
-          msg1.innerHTML = "Yahhhh you won it!!!";
-          msg2.innerHTML = "The number was " + answer;
-          msg3.innerHTML = "You guessed it in " + no_of_guesses + " guesses.";
+          msg1.innerHTML = "Yahhhh lo conseguiste!!!";
+          msg2.innerHTML = "El número era " + answer;
+          msg3.innerHTML = "Lo adivinaste en " + no_of_guesses + " intentos.";
           reset.innerHTML = "<button onclick=\"location.reload()\">RESET</button>";
         }
         else {
-          alert("Something went wrong!!");
+          alert("Algo fue mal!!");
         }
   }
 }
